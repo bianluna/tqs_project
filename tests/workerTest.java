dfccdd36bbe82be949458a799bfef78bc7d03a1b
bianca <bian.lunabian@gmail.com>
2025-10-13 15:50:35 +0200

    Initial version

---
+++ tests/workerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class workerTest {
+
+  @Test
+  void test(){
+    fail("Not yet implemented");
+  }
+
+
+
+}
