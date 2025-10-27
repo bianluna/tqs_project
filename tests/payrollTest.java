f6d261f4b949fea4cc159992b4eabb02cec2ef3b
bianca <bian.lunabian@gmail.com>
2025-10-27 10:31:55 +0100

    New branch creation and payrollTest class
    data as in civil status options, contract options and variables as in invalidWorker now emptyWorker
    
    Signed-off-by: bianca <bian.lunabian@gmail.com>

---
+++ tests/payrollTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.Worker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class payrollTest {
+}
