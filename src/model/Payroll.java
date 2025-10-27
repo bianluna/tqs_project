042b91b143b7be8cb5b34674812f74a168377796
bianca <bian.lunabian@gmail.com>
2025-10-27 11:19:35 +0100

    feat: constructor implementated in Payroll class
    Both parameted constructor and void constructor.
    
    Signed-off-by: bianca <bian.lunabian@gmail.com>

--- src/model/Payroll.java
+++ src/model/Payroll.java
@@ -1,6 +1,22 @@
 package model;
 
 public class Payroll {
+  public String workerName;
+  public float annualGrossSalary;
+  public int paymentsPerYear;
+
+  public Payroll() {
+    this.workerName = "";
+    this.annualGrossSalary = 0;
+    this.paymentsPerYear = 0;
+  }
+
+  public Payroll(float annualGrossSalary, String workerName, int paymentsPerYear) {
+    this.annualGrossSalary = annualGrossSalary;
+    this.workerName = workerName;
+    this.paymentsPerYear = paymentsPerYear;
+  }
+
 
 
 
