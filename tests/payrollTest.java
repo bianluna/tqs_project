ef819ede45a3426c886ef60396ad4c54b5e415ec
bianca <bian.lunabian@gmail.com>
2025-10-27 11:18:24 +0100

    test: payrollTest with constructor test.
    testConstructor defined in class payrollTest.
    
    Signed-off-by: bianca <bian.lunabian@gmail.com>

--- tests/payrollTest.java
+++ tests/payrollTest.java
@@ -1,8 +1,22 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import model.Worker;
+import model.Payroll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class payrollTest {
+  Payroll payroll = new Payroll();
+
+  @Test
+  void testConstructor() {
+    payroll = new Payroll(20000, "Pepito", 12);
+
+    assertEquals("Pepito", payroll.workerName);
+    assertTrue(payroll.annualGrossSalary == 20000);
+    assertTrue(payroll.paymentsPerYear == 12);
+
+  }
+
+
+
 }
