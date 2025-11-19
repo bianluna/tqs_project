e433f1ccfc27c7c198769ee77d457d9d1817722e
Bianca <bian.lunabian@gmail.com>
2025-11-19 11:47:58 +0100

    feat: constructor of Company implemented

---
+++ src/model/Company.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Company {
+  public String name;
+  public String cif;
+  public String email;
+  public String phone;
+  public String address;
+  public String website;
+  public String cnae;
+
+  public Company(String name, String cif, String email, String phone, String address, String website, String cnae) {
+    this.name = name;
+    this.cif = cif;
+    this.email = email;
+    this.phone = phone;
+    this.address = address;
+    this.website = website;
+    this.cnae = cnae;
+  }
+
+
+}
