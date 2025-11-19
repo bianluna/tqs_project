914a5b0a424948bdaa372112d04fd5fb53827b97
Bianca <bian.lunabian@gmail.com>
2025-11-19 11:45:09 +0100

    test: testing of company constructor

---
+++ tests/companyTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class companyTest {
+
+  @Test
+  void testCompanyConstructor() {
+    Company company = new Company("Tech Solutions", "123456789", "techsolutions@email.com", "555-1234", "123 Tech St, Silicon Valley", "www.techsolutions.com", "A03");
+    assertNotNull(company);
+    assertEquals(company.cnae, "A03");
+    assertEquals(company.name, "Tech Solutions");
+    assertEquals(company.cif, "123456789");
+  }
+
+
+}
