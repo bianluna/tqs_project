739f4f4232681b4202ae397b12eb9dcb3e1fe224
Bianca <bian.lunabian@gmail.com>
2025-12-03 15:00:28 +0100

    feat: add dni attribute to existing workers in WorkerRepositoryMock

--- tests/WorkerRepositoryMock.java
+++ tests/WorkerRepositoryMock.java
@@ -11,31 +11,31 @@ public class WorkerRepositoryMock {
 
   public WorkerRepositoryMock() {
     workers.add(new Worker(
-        "Juan Pérez", "Soltero", 0,
+        "Juan Pérez", "48392015S", "Soltero", 0,
         24000f, 14, "Indefinido", 3,
         "B12345678"
     ));
 
     workers.add(new Worker(
-        "Ana Torres", "Casada", 2,
+        "Ana Torres", "71239485K", "Casada", 2,
         28000f, 14, "Indefinido", 2,
         "B12345678"
     ));
 
     workers.add(new Worker(
-        "Luis Molina", "Soltero", 1,
+        "Luis Molina", "10293847J",  "Soltero", 1,
         22000f, 12, "Temporal", 4,
         "B34567890"
     ));
 
     workers.add(new Worker(
-        "María Varela", "Casada", 3,
+        "María Varela", "05672193W",  "Casada", 3,
         35000f, 14, "Indefinido", 1,
         "B34567890"
     ));
 
     workers.add(new Worker(
-        "Pedro Gómez", "Soltero", 0,
+        "Pedro Gómez", "99221188Q", "Soltero", 0,
         18000f, 12, "Parcial", 5,
         "B34567890"
     ));
